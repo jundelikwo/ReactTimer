3774649b379a8b3f65b2f638a0e75f127d0c5888
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2017-09-17 20:49:09 +0100

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Link, IndexLink} from 'react-router'
+
+var Nav = ()=> {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">React Time App</li>
+                    <li>
+                        <IndexLink to="/" activeClassName="activeLink">Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="activeLink">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <a href="https://github.com/jundelikwo" target="_blank">Jonathan Undelikwo</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+module.exports = Nav;
